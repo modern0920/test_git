72f9f602295d8498fc8160dfe91331b046e5a2d8
kdh <daekim95@naver.com>
2019-07-24 20:18:47 +0900

    메인액티비티 수정했음 -대현

---
+++ app/src/main/java/com/example/basicwidget/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.basicwidget;
+//Do it! 안드로이드 앱 프로그래밍 [개정4판&개정5판] - Day12-01
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RadioButton radioButton = (RadioButton) findViewById(R.id.radioButton);
+        boolean checked = radioButton.isChecked();
+    }
+
+
+}
